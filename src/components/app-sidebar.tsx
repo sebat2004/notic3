56c5bbbead76265f881741c035d52416d13b8e98
Kevin R <kevinru2005@gmail.com>
2024-10-18 22:01:24 -0700

    added base sidebar ?

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,21 +1,20 @@
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarHeader,
-} from '../components/ui/sidebar'
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarHeader,
+} from '../components/ui/sidebar';
 
 export function AppSidebar() {
-  return (
-    <Sidebar>
-      <SidebarHeader />
-      <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
-      </SidebarContent>
-      <SidebarFooter />
-    </Sidebar>
-  )
+    return (
+        <Sidebar>
+            <SidebarHeader />
+            <SidebarContent>
+                <SidebarGroup />
+                <SidebarGroup />
+            </SidebarContent>
+            <SidebarFooter />
+        </Sidebar>
+    );
 }
-
