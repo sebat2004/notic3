f40c911925093b8f93018543e9e5dda710189890
Seba <sebastiantorresola3@gmail.com>
2024-10-18 22:50:44 -0700

    Create Symmetric Key

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import { useEffect } from 'react';
 
 const CreatePage = () => {
+    const [key, setKey] = React.useState<CryptoKey | null>(null);
+    useEffect(() => {
+        (async () => {
+            const newKey = await crypto.subtle.generateKey(
+                {
+                    name: 'AES-GCM',
+                    length: 256, // Key length in bits
+                },
+                true, // Extractable
+                ['encrypt', 'decrypt'] // Key usages
+            );
+            setKey(newKey);
+        })();
+    }, []);
     return (
         <div>
             <h1>Create Page</h1>
