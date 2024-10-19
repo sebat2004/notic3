3dfe5f9a5a86b177aae573ae8c44a4b5c3e6b13b
owenkrause <owenjkrause@gmail.com>
2024-10-19 00:37:34 -0700

    added connect wallet button

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -7,33 +7,32 @@ import { AppSidebar } from '@/components/app-sidebar';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { Connect } from '@/components/connect';
 import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
-import { getFullnodeUrl } from '@mysten/sui/client'
+import { getFullnodeUrl } from '@mysten/sui/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactNode } from 'react';
 
 const queryClient = new QueryClient();
 
 const networks = {
-  testnet: { url: getFullnodeUrl('testnet') },
-  mainnet: { url: getFullnodeUrl('mainnet') },
+    testnet: { url: getFullnodeUrl('testnet') },
+    mainnet: { url: getFullnodeUrl('mainnet') },
 };
 
 export function Providers({ children }: { children: ReactNode }) {
-  return (
-    <QueryClientProvider client={queryClient}>
-        <SuiClientProvider networks={networks} defaultNetwork="testnet">
-            <WalletProvider>
-                
-                <SidebarProvider>
-                    <AppSidebar />
-                    <main>
-                        <SidebarTrigger />
-                        {children}
-                        <Connect />
-                    </main>
-                </SidebarProvider>
-            </WalletProvider>
-        </SuiClientProvider>
-    </QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <SuiClientProvider networks={networks} defaultNetwork="testnet">
+                <WalletProvider>
+                    <SidebarProvider>
+                        <AppSidebar />
+                        <main>
+                            <SidebarTrigger />
+                            {children}
+                            <Connect />
+                        </main>
+                    </SidebarProvider>
+                </WalletProvider>
+            </SuiClientProvider>
+        </QueryClientProvider>
+    );
 }
--- src/components/connect.jsx
+++ src/components/connect.jsx
@@ -1,9 +1,9 @@
 import { ConnectButton } from '@mysten/dapp-kit';
 
 export function Connect() {
-	return (
-		<div className="absolute top-4 right-6">
-			<ConnectButton />
-		</div>
-	);
+    return (
+        <div className="absolute right-6 top-4">
+            <ConnectButton />
+        </div>
+    );
 }
