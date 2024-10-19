c854d7aa621dfda6d52429cba579bbaa352a647a
Cassius Villareal <cassiusfive@gmail.com>
2024-10-19 14:56:06 -0700

    remove address requirement for explore

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,40 +1,13 @@
 'use client';
 
 import React from 'react';
-import { Separator } from '@/components/ui/separator';
-
-import { useCurrentAccount, useSuiClientQuery } from '@mysten/dapp-kit';
 import { CreatorList } from './components/creator-list';
 
 function Creators() {
-    const account = useCurrentAccount();
-
-    if (!account) {
-        return null;
-    }
-
     return (
-        <div className="mt-12 w-full px-10">
-            <h1 className="mb-4 text-center text-5xl font-bold">Featured Creators</h1>
+        <div className="my-12 w-full px-10">
+            <h1 className="mb-6 text-center text-5xl font-bold">Featured Creators</h1>
             <CreatorList />
-            <div>Connected to {account.address}</div>
-            <Objects address={''} />
-        </div>
-    );
-}
-
-function Objects({ address }: { address: string }) {
-    const { data } = useSuiClientQuery('getObject', {
-        id: address,
-    });
-
-    if (!data || !data.data) return <div>No data return</div>;
-
-    return (
-        <div key={data.data.objectId}>
-            <a href={`https://example-explorer.com/object/${data.data?.objectId}`}>
-                {data.data.objectId}
-            </a>
         </div>
     );
 }
--- src/components/connect.jsx
+++
@@ -1,9 +0,0 @@
-import { ConnectButton } from '@mysten/dapp-kit';
-
-export function Connect() {
-    return (
-        <div className="absolute right-6 top-4">
-            <ConnectButton />
-        </div>
-    );
-}
