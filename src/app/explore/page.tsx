ea353cf2e0b42eb51653609ebac7c4d1adf91644
owenkrause <owenjkrause@gmail.com>
2024-10-19 01:37:17 -0700

    added creator registry and page

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,11 +1,39 @@
+"use client"
+
 import React from 'react';
 
-const ExplorePage = () => {
-    return (
-        <div>
-            <h1>Explore Page</h1>
-        </div>
-    );
-};
 
-export default ExplorePage;
+import { useCurrentAccount, useSuiClientQuery } from '@mysten/dapp-kit';
+
+function Creators() {
+	const account = useCurrentAccount();
+
+	if (!account) {
+		return null;
+	}
+
+	return (
+		<div>
+			<div>Connected to {account.address}</div>;
+			<Objects address={""} />
+		</div>
+	);
+}
+
+function Objects({ address }: { address: string }) {
+	const { data } = useSuiClientQuery('getObject', {
+		id: address,
+	});
+
+	if (!data || !data.data) return <div>No data return</div>
+
+	return (
+        <div key={data.data.objectId}>
+            <a href={`https://example-explorer.com/object/${data.data?.objectId}`}>
+                {data.data.objectId}
+            </a>
+        </div>
+	);
+}
+
+export default Creators;
--- src/app/subscriptions/page.tsx
+++ src/app/subscriptions/page.tsx
@@ -1,11 +1,51 @@
+"use client"
+
 import React from 'react';
+import { useCurrentAccount, useSuiClientQuery } from '@mysten/dapp-kit';
 
-const SubscriptionsPage = () => {
-    return (
-        <div>
-            <h1>Subscriptions Page</h1>
-        </div>
-    );
-};
+const SUBSCRIPTION_TYPE = "PACKAGE_ADDRESS::subscription::Subscription"
 
-export default SubscriptionsPage;
+function Subscriptions() {
+	const account = useCurrentAccount();
+
+	if (!account) {
+		return null;
+	}
+
+	return (
+		<div>
+			<div>Connected to {account.address}</div>
+			<OwnedObjects address={account.address} />
+		</div>
+	);
+}
+
+function OwnedObjects({ address }: { address: string }) {
+	const { data } = useSuiClientQuery('getOwnedObjects', {
+		owner: address,
+        options: {
+            showType: true,
+            showContent: true,
+        },
+        filter: {
+            StructType: SUBSCRIPTION_TYPE
+        }
+	});
+
+	if (!data || data.data.length == 0) return <div>No data return</div>
+    if (data.data.length == 0) return <div>You do not have any subscriptions</div>
+
+	return (
+        <ul>
+            {data.data.map((object) => (
+                <li key={object.data?.objectId}>
+                    <a href={`https://suiscan.xyz/testnet/object/${object.data?.objectId}`} target="_blank">
+                        {object.data?.objectId}
+                    </a>
+                </li>
+            ))}
+        </ul>
+	);
+}
+
+export default Subscriptions;
