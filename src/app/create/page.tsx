ab3bef35bdadbca25b7d1178e5d988fc48ec5415
Seba <sebastiantorresola3@gmail.com>
2024-10-19 00:20:25 -0700

    Decrypt file uploads

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import { useEffect } from 'react';
 
@@ -16,9 +17,46 @@ const CreatePage = () => {
             setKey(newKey);
         })();
     }, []);
+
+    const handleUpload = async () => {
+        const file = (document.querySelector('input[type="file"]') as HTMLInputElement)?.files?.[0];
+        if (!file) {
+            return;
+        }
+
+        const fileData = await file.arrayBuffer();
+        const encryptedFile = await crypto.subtle.encrypt(
+            {
+                name: 'AES-GCM',
+                iv: new Uint8Array(12), // Initialization vector
+            },
+            key!,
+            fileData
+        );
+        const blob1 = new Blob([encryptedFile]);
+        const url = URL.createObjectURL(blob1);
+        const img = document.getElementById('img') as HTMLImageElement;
+        img.src = url;
+
+        // So the Blob can be Garbage Collected
+        img!.onload = (e) => URL.revokeObjectURL(url);
+
+        // TODO: Send the encrypted file to the Walrus API
+    };
+
     return (
         <div>
             <h1>Create Page</h1>
+            <p>{key ? 'Key created' : 'Creating key...'}</p>
+            <input type="file" />
+            <button
+                className="h-12 w-32 rounded-sm bg-gray-200"
+                onClick={handleUpload}
+                disabled={!key}
+            >
+                Upload File
+            </button>
+            <img id="img" />
         </div>
     );
 };
