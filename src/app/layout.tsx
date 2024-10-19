6afde3a796e9fe4a437ea2f7dff3951b3e340e23
Cassius Villareal <cassiusfive@gmail.com>
2024-10-19 16:04:05 -0700

    move fix layout logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,15 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
-import { Providers } from './providers';
 
 const geistSans = localFont({
-    src: './fonts/GeistVF.woff',
+    src: './(fonts)/GeistVF.woff',
     variable: '--font-geist-sans',
     weight: '100 900',
 });
 
 const geistMono = localFont({
-    src: './fonts/GeistMonoVF.woff',
+    src: './(fonts)/GeistMonoVF.woff',
     variable: '--font-geist-mono',
     weight: '100 900',
 });
@@ -28,7 +27,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                <Providers>{children}</Providers>
+                {children}
             </body>
         </html>
     );
