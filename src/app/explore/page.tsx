3c41400f9626a31b7676c7a869bbc3fcb2a77b93
owenkrause <owenjkrause@gmail.com>
2024-10-19 01:37:32 -0700

    added creator registry and page

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,39 +1,38 @@
-"use client"
+'use client';
 
 import React from 'react';
-
 
 import { useCurrentAccount, useSuiClientQuery } from '@mysten/dapp-kit';
 
 function Creators() {
-	const account = useCurrentAccount();
+    const account = useCurrentAccount();
 
-	if (!account) {
-		return null;
-	}
+    if (!account) {
+        return null;
+    }
 
-	return (
-		<div>
-			<div>Connected to {account.address}</div>;
-			<Objects address={""} />
-		</div>
-	);
+    return (
+        <div>
+            <div>Connected to {account.address}</div>;
+            <Objects address={''} />
+        </div>
+    );
 }
 
 function Objects({ address }: { address: string }) {
-	const { data } = useSuiClientQuery('getObject', {
-		id: address,
-	});
+    const { data } = useSuiClientQuery('getObject', {
+        id: address,
+    });
 
-	if (!data || !data.data) return <div>No data return</div>
+    if (!data || !data.data) return <div>No data return</div>;
 
-	return (
+    return (
         <div key={data.data.objectId}>
             <a href={`https://example-explorer.com/object/${data.data?.objectId}`}>
                 {data.data.objectId}
             </a>
         </div>
-	);
+    );
 }
 
 export default Creators;
