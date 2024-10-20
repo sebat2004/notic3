cf841c21e3f777ab4a6b1b743e31802c72558ae4
Cassius Villareal <cassiusfive@gmail.com>
2024-10-20 05:11:09 -0700

    fuck

--- src/hooks/use-creators.tsx
+++ src/hooks/use-creators.tsx
@@ -1,6 +1,6 @@
 import { useSuiClientQuery, useSuiClient } from '@mysten/dapp-kit';
 import { useQuery } from '@tanstack/react-query';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 export interface Creator {
     address: string; // crypto address
@@ -9,35 +9,47 @@ export interface Creator {
     image: string;
 }
 
-export const useCreators = async () => {
+export const useCreators = () => {
     const suiClient = useSuiClient();
+    const [creators, setCreators] = useState<Creator[]>([]);
 
-    const registryResp = await suiClient.getObject({
-        id: process.env.NEXT_PUBLIC_CREATOR_REGISTRY_ID,
-        options: {
-            showContent: true,
-        },
-    });
+    useEffect(() => {
+        const fetchCreators = async () => {
+            const registryResp = await suiClient.getObject({
+                id: process.env.NEXT_PUBLIC_CREATOR_REGISTRY_ID,
+                options: {
+                    showContent: true,
+                },
+            });
 
-    const ids = [];
-    registryResp.data.content.fields.creators.fields.contents.forEach((c) => {
-        ids.push(c.fields.value);
-    });
+            console.log(registryResp);
 
-    const usersResp = await suiClient.multiGetObjects({
-        ids,
-        options: {
-            showContent: true,
-        },
-    });
+            const ids: string[] = [];
+            registryResp!.data!.content!.fields.creators.fields.contents.forEach((c) => {
+                ids.push(c.fields.value);
+            });
 
-    const creators: Creator[] = useMemo(() => {
-        return usersResp.map((rawCreator: any) => ({
-            name: rawCreator.data.content.fields.value.fields.name as string,
-            bio: rawCreator.data.content.fields.value.fields.bio as string,
-            address: rawCreator.data.content.fields.creator_address as string,
-            image: rawCreator.data.content.fields.value.fields.picture as string,
-        }));
-    }, [usersResp]);
-    return { creators };
+            const usersResp = await suiClient.multiGetObjects({
+                ids,
+                options: {
+                    showContent: true,
+                },
+            });
+
+            console.log(usersResp);
+
+            setCreators(
+                usersResp.map((rawCreator: any) => ({
+                    name: rawCreator.data.content.fields.value.fields.name as string,
+                    bio: rawCreator.data.content.fields.value.fields.bio as string,
+                    address: rawCreator.data.content.fields.creator_address as string,
+                    image: rawCreator.data.content.fields.value.fields.picture as string,
+                }))
+            );
+        };
+
+        fetchCreators();
+    }, []);
+
+    return creators;
 };
